33f0d81930f13be2b943d0bf9f17d53571f572fe
danrok0 <79456088+danrok0@users.noreply.github.com>
2021-02-23 14:38:18 +0100

    wstepna

---
+++ Praktyki/src/com/company/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args) {
+  //      zadanie 1
+        /*
+        int A = 1;
+        double B = 4.3;
+        char C = 't';
+        String D = "sdsdsds";
+        System.out.println();
+
+
+         */
+        //zadanie 2
+        int myIntA = 2 * 2;
+        int myIntB = 3 * 3;
+
+
+
+        int sum = myIntA  + myIntB   ;
+
+
+        System.out.println("Wyniki: "  + sum);
+
+       //zadanie 3
+
+        int myIntC = 7 * 7;
+        double sqrt = Math.sqrt(3);
+        double myDoubleA = 49 * sqrt;
+        double sumDouble =  myDoubleA / 4;
+        System.out.println(+ sumDouble);
+
+
+
+
+
+        //zadanie 4
+
+        String a =  "Konrad";
+        String b = " Pieczykolan";
+        System.out.println(a + b);
+
+
+        System.out.println();
+    }
+}
