02ad2a5c038b37373d940d8996f63b438e3e257c
danrok0 <79456088+danrok0@users.noreply.github.com>
2021-02-25 10:41:43 +0100

    Update Main.java

--- Praktyki/src/com/loop/Main.java
+++ Praktyki/src/com/loop/Main.java
@@ -13,23 +13,20 @@ public class Main {
                 System.out.print(pierwszaLitera + "\t");
             pierwszaLitera++;
         }
-    }
-}
+    
+
 
         //Zadanie 2
 
-    class FibonacciExample1{
-    public static void main(String args[])
-    {
-        int n1=0,n2=1,n3,i,count=10;
-        System.out.print(n1+" "+n2);
-
-        for(i=2;i<count;++i)
-        {
-            n3=n1+n2;
-            System.out.print(" "+n3);
-            n1=n2;
-            n2=n3;
+    int f,g,h;
+f = g = 1;
+int numberOfCalculations = 10;
+System.out.print(f + "\t" + g + "\t");
+for (int i =0; i < numberOfCalculations;i++){
+    h = f + g;
+    f = g;
+    g = h;
+    System.out.print(h + "\t");
         }
 
-    }}
+    }
