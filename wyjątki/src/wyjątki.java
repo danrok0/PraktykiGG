a4cbb53eeb9a006e83d5ef4cb1dc9aa91217c3d0
danrok0 <79456088+danrok0@users.noreply.github.com>
2021-03-09 22:44:18 +0100

    wyjątki

---
+++ wyjątki/src/wyjątki.java
@@ -0,0 +1,50 @@
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class wyjątki {
+
+    public static void main(String[] args) {
+        //Zadanie 1
+        int[] array = new int[10];
+        Random random = new Random();
+        int intRandom;
+        try {
+            for (int i = 0; i < 11; i++) {
+                intRandom = random.nextInt(99);
+                array[i] = intRandom;
+                System.out.println(array[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+            System.out.println("Tablica jest już pełna nie można wpisać więcej danych");
+        }
+
+        //Zadanie 2
+        System.out.println();
+
+        try { int[] tablica = new int[4];
+            Scanner scanner = new Scanner(System.in);
+            try {
+                for (int i = 0; i < 5; i++) {
+                    tablica[i] = scanner.nextInt();
+
+                    System.out.println("Dla i=" + i + " Przypisałeś wartość " + tablica[i]);
+                }
+            } catch (InputMismatchException inputMismatchException) {
+                System.out.println("Niepoprawne dane!");
+
+            } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {System.out.println("Tablica jest pełna!");
+            }
+
+        }finally {
+            System.out.println("Koniec programu!");
+        }
+    }
+}
+
+
+
+
+
+
+
