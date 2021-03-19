40a9ab91a472e5e39a8f7d72ba88e49aa11afb1b
danrok0 <79456088+danrok0@users.noreply.github.com>
2021-03-19 07:47:20 +0100

    Update main.java

--- projekt_praktyki/src/main.java
+++ projekt_praktyki/src/main.java
@@ -94,7 +94,7 @@ class Main {
     }
 
     public static void drawBoard(char[][] board) {
-        System.out.println("Tablica:");
+        System.out.println("Plansza:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j]);
